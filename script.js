a4231d072650e63cebddf554c79b680ee62d6454
Harald <Harald@espinagar.se>
2022-12-05 20:43:11 +0100

    tried to make the pictures display in random order one last time but not quite getting it right

--- script.js
+++ script.js
@@ -1,45 +1,5 @@
-/*
-Hygienkrav
-Nedan hygienkrav ska vara uppfyllda oavsett betygsnivå.
 
-vara responsiv (mobile first, minst 3 olika breakpoints)
-semantiskt korrekt
-använda flexboxes alternativt CSS grid (går så klart bra att använda Bootstrap/Tailwind etc.)
-all data och status ska finnas i JavaScript, dvs ska ej använda DOM som ”single point of truth”
-spelet ska vara publicerat på Netlify
- 
-
-Versionshantering
-(Bedöms i kursen Arbetsmetodik för utvecklare) All källkod ska vara versionshanterad i ett (privat) Git-repository, där ni löpande gör commits med beskrivande commit-meddelanden, så att det är enkelt att följa ert utvecklingsarbete.
-
- 
-
-Struktur
-Det ska finnas en Array med objekt, där varje objekt ska ha egenskaperna name och image, där name är en sträng med namnet på personen och image är en URL till bilden på personen.
-
-Array:en ska INTE innehålla någon information som ex. svarsalternativ eller vilket svar som är rätt! Däremot går det bra att lägga till ett id på varje person, om du skulle vilja.
-
-Array:en ska kunna innehålla obegränsat antal personer (dvs du ska aldrig förutsätta att array:en är en viss längd!). När spelet startar/startar om ska man kunna välja hur många personer man vill gissa namnen för (tex. 10, 20 eller samtliga).
-
-För varje person ska hens foto visas, samt fyra svarsalternativ bestående av personens namn (rätt svar) samt tre slumpmässigt utvalda andra personers namn. Platsen för rätt svar ska vara slumpmässig så man inte kan ”fuska” genom att lära sig var rätt svar är placerat. Naturligtvis ska inte samma namn heller förekomma två gånger för samma foto.
-
-Efter att man valt ett svarsalternativ på varje person, så ska resultatet visas (alt. om man trycker på en knapp ”Visa resultat”), samt efter varje spelrunda där man får reda på hur många rätt vs. fel man hade (t.ex. 16/20 rätt om man gissade rätt på 16 av 20 personer).
-
- 
-
-Deadline
-Inlämning ska ske senast måndag 5 december kl. 23:55 under Inlämningsuppgift 1 för kursen JavaScript Grundkurs – FED22M i Itslearning.
-
-Glöm inte bjuda in mig (drblue) som collaborator på GitHub!
-
-Skicka in
-
-Länk till ditt GitHub-repo
-Länk till ditt publicerade spel
-Om du gjort G eller VG-nivå
-Ev. kända buggar eller kommentarer du tror jag skulle ha nytta av när jag granskar din inlämning
-*/
-
+//QUERY SELECTORS
 const picEl = document.querySelector("#pic");
 const startEl = document.querySelector("#start");
 const namesEl = document.querySelector("#names");
@@ -98,12 +58,18 @@ let corrClassmate = "";
 let corrName = "";
 let allCorrect = [];
 let allWrong = [];
+let namesNotToShow = [];
+
+// const newClassmate = () => {
+//   arrayCopy = array.filter(array => array.name !== namesNotToShow[guesses].name[2]);
+//   return arrayCopy
+// }
 
 // START function
 const start = () => {
   if (guesses !== array.length) {
     // array = array.filter(array => array.name !== namesNotToShow);
-    corrClassmate = array[guesses]  
+    corrClassmate = arrayCopy[guesses]
     picEl.src = "students/" + corrClassmate.image
     corrName = corrClassmate.name
     arrayCopy = array.filter(array => array.name !== corrName);
@@ -114,7 +80,8 @@ const start = () => {
     //GENERATE NEW ARRAY
     newRandomArr = arrayCopy.slice(0, 3);
     newRandomArr.push({name: corrName});
-    console.log(allWrong)
+    namesNotToShow.push({name: newRandomArr});
+    console.log(namesNotToShow)
     //SHUFFFLE NEW ARRAY AGAIN SO RIGHT BUTTON ISN'T AT THE SAME SPOT EVERY TIME 
     shuffleArr(newRandomArr);
 
